1c579a3ca07f2fbf2b0e492e11f5dc3f70eaacac
Volodymyr Kysenko <mnrb73@motorola.com>
2013-06-24 20:25:53 -0700

    -- added example with histogram

--- src/imaje/core.clj
+++ src/imaje/core.clj
@@ -27,18 +27,29 @@
   ([image]
     (.getDataElements (.getRaster image) 0 0 (.getWidth image) (.getHeight image) nil)))
 
-(defn set-pixels 
+(defn set-pixels!
   ""
   ([image pixels]
     (.setDataElements (.getRaster image) 0 0 (.getWidth image) (.getHeight image) (int-array pixels))))
 
+(defn pixel-map
+	""
+	[fn pixels]
+	(map fn pixels)
+	)
+
+(defn pixel-reduce
+	""
+	[fn val pixels]
+	(reduce fn val pixels)
+	)
+
 (defn -main
 	[]
 	(println "Starting test application")
-	(let [img (empty-image 640 480)
-			pixels (get-pixels img)]
-		; (set-pixels img pixels)
-		(set-pixels img (map #(+ % 255) pixels))
+	(let [img (empty-image 640 480)]
+		(set-pixels! img (pixel-map #(+ % 255) (get-pixels img)))
+		(println (pixel-reduce #(update-in %1 [%2] inc) (vec (repeat 256 0)) (get-pixels img)))
 		(save-image img "out.png")
 		)
 )
