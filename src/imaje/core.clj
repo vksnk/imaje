fb3725428380c6b18d1a1c0a23bb4985f9b7e8db
Volodymyr Kysenko <mnrb73@motorola.com>
2013-06-26 12:58:35 -0700

    -- separated wrapper around BufferedImage

--- src/imaje/core.clj
+++ src/imaje/core.clj
@@ -1,52 +1,8 @@
 (ns imaje.core
-	(import java.io.File)
-	(import java.awt.image.BufferedImage)
-	(import javax.imageio.ImageIO)
+	(:use [imaje.wrapper])
 	)
 
 ; (set! *warn-on-reflection* true)
-
-(defn width
-	""
-	[image]
-	(.getWidth image)
-	)
-
-(defn height
-	""
-	[image]
-	(.getHeight image)
-	)
-
-(defn empty-image
-	""
-	([^BufferedImage image]
-	(empty-image (width image) (height image)))
-	([wd hg]
-	(BufferedImage. wd hg BufferedImage/TYPE_INT_RGB)
-	))
-
-(defn load-image
-	""
-	[filename]
-	(ImageIO/read (File. filename))
-	)
-
-(defn save-image
-	""
-	[img filename]
-	(ImageIO/write img "png" (File. filename))
-	)
-
-(defn get-pixels 
- 	""
-  	([image]
-    	(.getDataElements (.getRaster image) 0 0 (width image) (height image) nil)))
-
-(defn set-pixels!
- 	""
-  	([image pixels]
-    	(.setDataElements (.getRaster image) 0 0 (.getWidth image) (.getHeight image) (int-array pixels))))
 
 (defn create-sampler
 	""
@@ -111,6 +67,11 @@
 			)
 		)
 	)		
+
+; (defn imscan
+; 	""
+; 	[fun image]
+; 	)
 
 (defn pixel-map
 	""
---
+++ src/imaje/wrapper.clj
@@ -0,0 +1,47 @@
+(ns imaje.wrapper
+	(import java.io.File)
+	(import java.awt.image.BufferedImage)
+	(import javax.imageio.ImageIO)
+	)
+
+(defn width
+	""
+	[image]
+	(.getWidth image)
+	)
+
+(defn height
+	""
+	[image]
+	(.getHeight image)
+	)
+
+(defn empty-image
+	""
+	([^BufferedImage image]
+	(empty-image (width image) (height image)))
+	([wd hg]
+	(BufferedImage. wd hg BufferedImage/TYPE_INT_RGB)
+	))
+
+(defn load-image
+	""
+	[filename]
+	(ImageIO/read (File. filename))
+	)
+
+(defn save-image
+	""
+	[img filename]
+	(ImageIO/write img "png" (File. filename))
+	)
+
+(defn get-pixels 
+ 	""
+  	([image]
+    	(.getDataElements (.getRaster image) 0 0 (width image) (height image) nil)))
+
+(defn set-pixels!
+ 	""
+  	([image pixels]
+    	(.setDataElements (.getRaster image) 0 0 (.getWidth image) (.getHeight image) (int-array pixels))))
