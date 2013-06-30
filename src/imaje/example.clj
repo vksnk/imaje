6730bd0a80fbc9a2f37673680509eb51b500f454
Volodymyr Kysenko <rizen.wp@gmail.com>
2013-06-29 21:48:53 -0700

    -- added couple of small functions

--- src/imaje/example.clj
+++ src/imaje/example.clj
@@ -1,7 +1,37 @@
 (ns imaje.example
-	(:use [imaje.wrapper]
-			[imaje.core])
-	)
+	(:use 
+		[imaje.core]
+		[imaje.wrapper]))
+
+; (defn average3x3
+; 	[x y sampler]
+; 	; (*
+; 		(sampler x y))
+; 		; (+ 
+; 			; (sampler (- x 1) (- y 1))
+; 			; (sampler x (- y))
+; 			; (sampler (+ x 1) (- y 1))
+
+; 			; (sampler (- x 1) y)
+; 			; (sampler x y)
+; 			; (sampler (+ x 1) y)
+
+; 			; (sampler (- x 1) (+ y 1))
+; 			; (sampler x (+ y 1))
+; 			; (sampler (+ x 1) (+ y 1))
+; 		; ))
+; 		 ; 9))
+
+(defn box-blur
+	[]
+	(let [
+			input-image (load-image "resources/yellow_square.png")]
+			; input-image (imrender (fn [x y] (rand-int 256)) 640 480)]
+			; blurred-image (immap average3x3 input-image)]
+			(println (get-type input-image))
+			(println (get-pixels input-image))
+			(save-image input-image "baboon_face_box_blur.png")))
+
 
 (defn -main
 	[]
--- src/imaje/wrapper.clj
+++ src/imaje/wrapper.clj
@@ -39,3 +39,20 @@
  	""
   	([image pixels]
     	(.setDataElements (.getRaster image) 0 0 (.getWidth image) (.getHeight image) (int-array pixels))))
+
+(defn get-type
+	"Returns a type of an image"
+	[image]
+	(.getType image))
+
+(defn get-rgb
+	"Returns an integer pixel in the default RGB color model"
+	[^BufferedImage image x y]
+	(.getRGB image x y)
+	)
+
+(defn set-rgb!
+	"Sets a pixel in this BufferedImage to the specified RGB value."
+	[^BufferedImage image x y rgb]
+	(.setRGB image x y rgb)
+	)
